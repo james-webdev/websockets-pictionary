d5dedb16b230df16c09dbcf43318d8511650fc3f
James Roe <58564719+james-webdev@users.noreply.github.com>
2020-09-30 10:13:23 +0200

    latest version

--- public/sketch.js
+++ public/sketch.js
@@ -4,7 +4,7 @@ let buttonClear;
 function setup() {
   createCanvas(600, 600);
   background("black");
-  socket = io.connect("http://127.0.0.1:8080");
+  socket = io.connect("https://damp-woodland-05737.herokuapp.com:8000");
 
   socket.on("mouse", (data) => {
     line(data.x, data.y, data.a, data.b);
